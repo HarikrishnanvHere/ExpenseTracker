0ce91600a314b34ffc2eb52e37a311ac8504e030
HarikrishnanHere <151928429+HarikrishnanvHere@users.noreply.github.com>
2024-01-28 16:51:51 +0530

    Add files via upload
    Back end of login page in Expense Tracker App Included

---
+++ Expense Tracker Login Backend/Back End/controllers/user.js
@@ -0,0 +1,38 @@
+let User = require('../models/user');
+
+let cors = require('cors');
+
+let express = require('express');
+let app = express();
+app.use(cors());
+
+exports.postSIgnUpUser = (req,res,next) =>{
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log(name,email,password);
+    User.create({
+        name: name,
+        email: email,
+        password: password
+    })
+    .then((data)=>{
+        res.send(data);
+    })
+    .catch(err=>res.send(err))
+}
+
+exports.postLogInUser = (req,res,next) =>{
+    let email = req.body.email;
+    let password = req.body.password;
+    User.findByPk(email)
+    .then((data)=>{
+        if(data.password === password){
+            res.send("LogIn Successful");
+        }
+        else{
+            res.send("Error Code 401 - User Not Authorized!");
+        }
+    })
+    .catch(err=>res.send("Error Code 404 - User Not Found!"));
+}
